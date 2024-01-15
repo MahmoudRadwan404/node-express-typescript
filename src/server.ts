94a6662de9d4ae0017143738d422fc42f8511443
MahmoudRadwan404 <mahmoudrdwan32@gmail.com>
2024-01-15 14:13:12 +0200

    done

--- src/server.ts
+++ src/server.ts
@@ -8,17 +8,17 @@ import fileUpload from "express-fileupload";
 //const fileUpload = require("express-fileupload");
 import { options } from "./core/options-cors";
 const app = express();
-app.use(cors());
+app.use(cors(options));
 app.use(fileUpload());
 const myPath = path.join(process.cwd() + "/storage/uploads");
 
 app.use(bodyParser.json());
-app.use(express.json());
+app.use(express.json())
 app.use("/uploads", express.static(myPath));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.urlencoded());
 
-app.listen(port, () => {
+app.listen(3000, () => {
   console.log("listening");
   console.log(myPath);
 });
